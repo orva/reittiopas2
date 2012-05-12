67e05f67d78a88a2ef77b7a3ad976485197799b7
Iivari Äikäs <iivari.aikas@gmail.com>
2012-05-12 17:21:13 +0300

    Base API connection.

--- lib/reittiopas2.rb
+++ lib/reittiopas2.rb
@@ -1,5 +1,12 @@
 require "reittiopas2/version"
 
-module Reittiopas2
-  # Your code goes here...
+class Reittiopas2
+  def initialize(username, password)
+    @username = username
+    @password = password
+  end
+
+  def base_api_url
+    "http://api.reittiopas.fi/hsl/prod/?user=#{@username}&pass=#{@password}"
+  end
 end
