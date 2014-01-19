868ce3d2f04d0121cf37a96aaa40d6ad404f408b
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2014-01-19 17:07:11 +0200

    Clean up indentation
    Follow the convention of one indentation level per enclosed class
    or module.

--- lib/reittiopas2/client.rb
+++ lib/reittiopas2/client.rb
@@ -6,53 +6,53 @@ require 'net/http'
 
 class Reittiopas2
 
-# Handles connectting to Reittiopas API and transforms query hashes into form
-# that Reittiopas API can digest them. This class is also responsible for
-# parsing response into Ruby readable form.
-#
-# @api private
-class Client
-  include Utilities
-
-  def initialize(username, password)
-    @base_url = "http://api.reittiopas.fi/hsl/prod/"
-    @base_query = {"user" => username, "pass" => password}
-  end
-
-  # Forms proper query from credentials + given query and sends it to API
-  # endpoint. Also parses response into Ruby readable form.
+  # Handles connectting to Reittiopas API and transforms query hashes into form
+  # that Reittiopas API can digest them. This class is also responsible for
+  # parsing response into Ruby readable form.
   #
-  # In case of errors, be those from API endpoint or from internal processes,
-  # return value will be hash with one field 'error' which contains error
-  # message.
-  #
-  # In fatal error situations Exception will be thrown, only exception for this
-  # rule is JSON::ParserException which is caught and returned as value of
-  # 'error' field.
-  #
-  # @param [Hash] query the query which is to be sent to API endpoint
-  # @return [Hash] the whatever data was requested from API, or Hash containing
-  #   key 'error' containing error message.
-  def perform_query(query={})
-    query = convert_array_values(query)
-    uri = Addressable::URI.parse(@base_url)
-    uri.query_values = @base_query.merge(query)
+  # @api private
+  class Client
+    include Utilities
 
-    res = Net::HTTP.get_response(uri)
-
-    if res.code == '500'
-      # In case of errors, code 500 is returned and there is explanation in
-      # response body as HTML.
-      {'error' => res.body}
-    elsif res.body.nil? or res.body.empty?
-      {'error' => "Response body was empty!"}
-    else
-      JSON.load res.body.to_s
+    def initialize(username, password)
+      @base_url = "http://api.reittiopas.fi/hsl/prod/"
+      @base_query = {"user" => username, "pass" => password}
     end
-  rescue JSON::ParserError => ex
-    {'error' => ex.class}
-  end
 
-end
+    # Forms proper query from credentials + given query and sends it to API
+    # endpoint. Also parses response into Ruby readable form.
+    #
+    # In case of errors, be those from API endpoint or from internal processes,
+    # return value will be hash with one field 'error' which contains error
+    # message.
+    #
+    # In fatal error situations Exception will be thrown, only exception for this
+    # rule is JSON::ParserException which is caught and returned as value of
+    # 'error' field.
+    #
+    # @param [Hash] query the query which is to be sent to API endpoint
+    # @return [Hash] the whatever data was requested from API, or Hash containing
+    #   key 'error' containing error message.
+    def perform_query(query={})
+      query = convert_array_values(query)
+      uri = Addressable::URI.parse(@base_url)
+      uri.query_values = @base_query.merge(query)
+
+      res = Net::HTTP.get_response(uri)
+
+      if res.code == '500'
+        # In case of errors, code 500 is returned and there is explanation in
+        # response body as HTML.
+        {'error' => res.body}
+      elsif res.body.nil? or res.body.empty?
+        {'error' => "Response body was empty!"}
+      else
+        JSON.load res.body.to_s
+      end
+    rescue JSON::ParserError => ex
+      {'error' => ex.class}
+    end
+
+  end
 
 end
--- lib/reittiopas2/geocoding.rb
+++ lib/reittiopas2/geocoding.rb
@@ -2,109 +2,108 @@ require 'reittiopas2/utilities'
 
 class Reittiopas2
 
-# Module containing public API to perform geocoding and reverse geocoding
-# operations.
-#
-# Location info returned from geocode methods is following form:
-# @example geocode location info
-#   [ 
-#     {
-#       'locType' => 'stop',
-#       'locTypeId' => '10',
-#       'name' => 'Elielinaukio',
-#       'matchedName' => 'Elielinaukio', 
-#       'lang' => 'fi',
-#       'city' => 'Helsinki',
-#       'coords' => '2552335,6673660',
-#       'details' => {
-#           'address' => 'Elielinaukio',
-#           'code' => '1020128',
-#           'shortCode' => '2020',
-#           'lines' => [
-#               '1015A 1:Länsiterminaali',
-#               '1015A 2:Elielinaukio'
-#           ]
-#       }
-#     }
-#   ]
-#
-#
-# @example reverse_geocode location info
-#   [ 
-#     {
-#       'locType' => 'address',
-#       'locTypeId' => '900',
-#       'name' => 'Purotie 8, Helsinki',
-#       'matchedName' => nil, 
-#       'lang' => 'fi',
-#       'city' => 'Helsinki',
-#       'coords' => '2552335,6673660',
-#       'distance' => '38.4187454245971',
-#       'details' => {
-#           'address' => nil,
-#           'houseNumber' => '8',
-#       }
-#     }
-#   ]
-#
-module Geocoding
-  include Utilities
+  # Module containing public API to perform geocoding and reverse geocoding
+  # operations.
+  #
+  # Location info returned from geocode methods is following form:
+  # @example geocode location info
+  #   [ 
+  #     {
+  #       'locType' => 'stop',
+  #       'locTypeId' => '10',
+  #       'name' => 'Elielinaukio',
+  #       'matchedName' => 'Elielinaukio', 
+  #       'lang' => 'fi',
+  #       'city' => 'Helsinki',
+  #       'coords' => '2552335,6673660',
+  #       'details' => {
+  #           'address' => 'Elielinaukio',
+  #           'code' => '1020128',
+  #           'shortCode' => '2020',
+  #           'lines' => [
+  #               '1015A 1:Länsiterminaali',
+  #               '1015A 2:Elielinaukio'
+  #           ]
+  #       }
+  #     }
+  #   ]
+  #
+  #
+  # @example reverse_geocode location info
+  #   [ 
+  #     {
+  #       'locType' => 'address',
+  #       'locTypeId' => '900',
+  #       'name' => 'Purotie 8, Helsinki',
+  #       'matchedName' => nil, 
+  #       'lang' => 'fi',
+  #       'city' => 'Helsinki',
+  #       'coords' => '2552335,6673660',
+  #       'distance' => '38.4187454245971',
+  #       'details' => {
+  #           'address' => nil,
+  #           'houseNumber' => '8',
+  #       }
+  #     }
+  #   ]
+  #
+  module Geocoding
+    include Utilities
 
-  # Search for information about place.
-  #
-  # @param [String] place_name the name of place to be searched. This can be
-  #   street name, point of interest, bus stop, etc.
-  # @param [Hash] opts the optional parameters which can be used to alter search
-  #   results. Any invalid keys are removed from query parameters, but values
-  #   are never checked.
-  #
-  # @option opts [String] 'cities' ('all') List of cities which are accepted.
-  # @option opts [String] 'loc_types' ('all') List of location types which are
-  #   accepted.
-  # @option opts [Integer] 'disable_error_correction' (0) Disable levenshtein 
-  #   error correction. 1 = Error correction NOT in use, 0 = error correction in
-  #   use.
-  # @option opts [Integer] 'disable_unique_stop_names' (1) Disable unique stop
-  #   names in the result. 1 = all stops are shown in the result, 0 = only one
-  #   stop is included in the result for stops with same name.
-  #
-  # @return [Array<Hash>] array containing location hashes matched given query.
-  # @see #reverse_geocode
-  def geocode(place_name, opts={})
-    clean = select_keys(opts, GEOCODE_KEYS)
-    query = {'request' =>'geocode', 'key' => place_name}.merge(clean)
-    @client.perform_query(query)
+    # Search for information about place.
+    #
+    # @param [String] place_name the name of place to be searched. This can be
+    #   street name, point of interest, bus stop, etc.
+    # @param [Hash] opts the optional parameters which can be used to alter search
+    #   results. Any invalid keys are removed from query parameters, but values
+    #   are never checked.
+    #
+    # @option opts [String] 'cities' ('all') List of cities which are accepted.
+    # @option opts [String] 'loc_types' ('all') List of location types which are
+    #   accepted.
+    # @option opts [Integer] 'disable_error_correction' (0) Disable levenshtein 
+    #   error correction. 1 = Error correction NOT in use, 0 = error correction in
+    #   use.
+    # @option opts [Integer] 'disable_unique_stop_names' (1) Disable unique stop
+    #   names in the result. 1 = all stops are shown in the result, 0 = only one
+    #   stop is included in the result for stops with same name.
+    #
+    # @return [Array<Hash>] array containing location hashes matched given query.
+    # @see #reverse_geocode
+    def geocode(place_name, opts={})
+      clean = select_keys(opts, GEOCODE_KEYS)
+      query = {'request' =>'geocode', 'key' => place_name}.merge(clean)
+      @client.perform_query(query)
+    end
+
+    # Do reverse geocode search to find information about given coordinates.
+    #
+    # @param [String] coords the coordinate pair "<x_coordinate>,<y_coordinate>"
+    # @param [Hash] opts the optional parameters which can be used to alter query
+    #   results. Any invalid keys are removed from query parameters, but values
+    #   are never checked.
+    #
+    # @option opts [Integer] 'limit' (1) Limit for the number of locations
+    #   returned. 
+    # @option opts [Integer] 'radius' (1000) Radius of the search in meters. Range
+    #   1-1000.
+    # @option opts [String] 'result_contains' ('address') Limit the search to
+    #   given location types. Possible values are: address, stop, poi
+    #
+    # @return [Array<Hash>] array containing location hashes matching given
+    #   coordinates.
+    # @see #geocode
+    def reverse_geocode(coords, opts={})
+      clean = select_keys(opts, REVERSE_GEOCODE_KEYS)
+      query = {'request' => 'reverse_geocode', 'coordinate' => coords}.merge(clean)
+      @client.perform_query(query)
+    end
+
+    GEOCODE_KEYS = ['key', 'cities', 'loc_types', 'disable_error_correction',
+      'disable_unique_stop_names']
+
+    REVERSE_GEOCODE_KEYS = ['coordinate', 'limit', 'radius', 'result_contains']
+
   end
-
-  # Do reverse geocode search to find information about given coordinates.
-  #
-  # @param [String] coords the coordinate pair "<x_coordinate>,<y_coordinate>"
-  # @param [Hash] opts the optional parameters which can be used to alter query
-  #   results. Any invalid keys are removed from query parameters, but values
-  #   are never checked.
-  #
-  # @option opts [Integer] 'limit' (1) Limit for the number of locations
-  #   returned. 
-  # @option opts [Integer] 'radius' (1000) Radius of the search in meters. Range
-  #   1-1000.
-  # @option opts [String] 'result_contains' ('address') Limit the search to
-  #   given location types. Possible values are: address, stop, poi
-  #
-  # @return [Array<Hash>] array containing location hashes matching given
-  #   coordinates.
-  # @see #geocode
-  def reverse_geocode(coords, opts={})
-    clean = select_keys(opts, REVERSE_GEOCODE_KEYS)
-    query = {'request' => 'reverse_geocode', 'coordinate' => coords}.merge(clean)
-    @client.perform_query(query)
-  end
-
-  GEOCODE_KEYS = ['key', 'cities', 'loc_types', 'disable_error_correction',
-    'disable_unique_stop_names']
-
-  REVERSE_GEOCODE_KEYS = ['coordinate', 'limit', 'radius', 'result_contains']
-
-end
-
 
 end
